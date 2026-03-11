340ba6c0f3b20e15398a5fecdcbf1c4aaaedc7e1
MucaL00 <vasconcelloskelisamuel@gmail.com>
2026-03-11 16:59:47 -0300

    comparador E (and) e ou (or)

---
+++ 05-operadoreslogicos.js
@@ -0,0 +1,30 @@
+//operadores lógicos
+let souPobre = true;
+
+console.log("sou pobre?", souPobre);
+console.log("Negação de sou pobre:", !souPobre);
+
+let manha = true;
+let sono = true;
+console.log("----------------------------------------");
+console.log("Manhã:", manha, "|estou com sono?", sono);
+
+// comparador E (and) e ou (or)
+console.log("operador AND (e):", manha && sono);
+console.log("operador OR (ou):", manha || sono);
+
+manha = false;
+console.log("----------------------------------------");
+console.log("Manhã:", manha, "|estou com sono?", sono);
+
+// comparador E (and) e ou (or)
+console.log("operador AND (e):", manha && sono);
+console.log("operador OR (ou):", manha || sono);
+
+sono = false;
+console.log("----------------------------------------");
+console.log("Manhã:", manha, "|estou com sono?", sono);
+
+// comparador E (and) e ou (or)
+console.log("operador AND (e):", manha && sono);
+console.log("operador OR (ou):", manha || sono);
