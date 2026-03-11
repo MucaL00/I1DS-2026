7f8b3cd9acf98c5883d3839bfc5fee61477ecb04
MucaL00 <vasconcelloskelisamuel@gmail.com>
2026-03-11 14:32:04 -0300

    criando sistema bancário simples

---
+++ atividades/01-sistemabancario.js
@@ -0,0 +1,43 @@
+/***************************************************** 
+Exercícios para fixação 
+*****************************************************/
+/* 
+1. Crie uma variável e atribua o nome de uma pessoa; 
+2. Crie uma variável e atribua o nome de um banco; 
+3. Crie uma variável e atribua o número da agência; 
+4. Crie uma variável e atribua o número da conta; 
+5. Crie uma variável e atribua o saldo da conta; 
+6. Imprima os dados e o saldo da conta; 
+7. Efetue e imprima 5 movimentações na conta; 
+8. Imprima novamente os dados e o saldo da conta; 
+*/
+let name = "Samuel";
+let bank = "NuBank";
+let agency = 1738;
+let account = 67;
+let balance = 676767;
+
+console.log("-------------------------");
+console.log("Nome do cliente:", name);
+console.log("Banco:", bank);
+console.log("número da agência:", agency);
+console.log("Número da conta:", account);
+console.log("saldo da conta:R$", balance);
+console.log("----------------------------------------------------------------");
+console.log("pix enviado a jãosmash no valor de R$89000\n");
+
+console.log("                    TRANSAÇÃO APROVADA!");
+console.log("sua transação foi aprovada para benqx no valor de R$10\n");
+
+console.log("seu dinheiro foi colocado na caixinha com sucesso!\n");
+
+console.log("pix recebido de Daniel Fraga no valor de 500.000\n");
+
+console.log("Seu plano spotify familiar foi pago(R$49,90).\n");
+
+console.log("----------------------------------------------------------------");
+console.log("Nome do cliente:", name);
+console.log("Banco:", bank);
+console.log("número da agência:", agency);
+console.log("Número da conta:", account);
+console.log("saldo da conta:R$", balance - 89.0 - 10 + 500.000 - 49.9);
